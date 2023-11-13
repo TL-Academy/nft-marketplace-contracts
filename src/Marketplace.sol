ef238de1c030df9335e28084a00a2eafd213f7f3
Darin Ivanov <d.v.ivanov98@gmail.com>
2023-11-13 14:28:05 +0200

    address initial comments by lead

--- src/Marketplace.sol
+++ src/Marketplace.sol
@@ -5,28 +5,37 @@ import "./NFT.sol";
 
 contract Marketplace {
 
-    event NFTListed(address indexed _listedBy, address indexed _collection, uint256 _id);
-    event NFTDelisted(address indexed _deletedBy, address indexed _collection, uint256 _id);
-    event NFTUpdated(address indexed _updatedBy, address indexed _collection, uint256 _id);
+    event NFTListed(address indexed listedBy, address indexed collection, uint256 indexed price, uint256 id);
+    event ListingCanceled(address indexed collection, uint256 id);
+    event ListingUpdated(address indexed collection, uint256 indexed newPrice, uint256 id);
     event NFTTransfered(
-        address indexed _transferedFrom, 
-        address indexed _transferedTo, 
-        address _collection, 
-        uint256 _id
+        address indexed transferedFrom, 
+        address indexed transferedTo, 
+        address collection, 
+        uint256 id
     );
 
     // contract -> (id -> price)
-    mapping (address =>  mapping(uint256 => uint256)) private NFTPrices;
+    mapping (address =>  mapping(uint256 => uint256)) private s_NFTPrices;
+
+    error notOwner();
+    error itemAlreadyListed();
+    error itemNotListed();
+    error priceIsZero();
+    error newPriceNotHigher();
+    error couldntPayOldOwner();
+    error marketplaceIsNotApproved();
+    error incorectPriceProvided();
 
     modifier isNFTOwner(address _collection, uint256 _id) {
-        require(NFT(_collection).ownerOf(_id) == msg.sender, "Not the owner!");
+        if (NFT(_collection).ownerOf(_id) != msg.sender) revert notOwner();
         _;
     }    
 
     // listed true -> revert if already listed. false -> revert if not listed
     modifier alreadyListed(address _collection, uint256 _id, bool listed) {
-        if (listed && NFTPrices[_collection][_id] != 0) revert("Already listed");
-        if (!listed && NFTPrices[_collection][_id] == 0) revert("Item not listed");
+        if (listed && s_NFTPrices[_collection][_id] != 0) revert itemAlreadyListed();
+        if (!listed && s_NFTPrices[_collection][_id] == 0) revert itemNotListed();
         _;
     }
 
@@ -40,9 +49,9 @@ contract Marketplace {
     isNFTOwner(_collection, _id)
     alreadyListed(_collection, _id, true)
     {
-        require(_price!=0, "Price should be more than 0");
-        NFTPrices[_collection][_id] = _price;
-        emit NFTListed(msg.sender, _collection, _id);
+        if(_price==0) revert priceIsZero();
+        s_NFTPrices[_collection][_id] = _price;
+        emit NFTListed(msg.sender, _collection, _price, _id);
     } 
 
 
@@ -54,8 +63,8 @@ contract Marketplace {
     isNFTOwner(_collection, _id)
     alreadyListed(_collection, _id, false)
     {
-        delete NFTPrices[_collection][_id];
-        emit NFTDelisted(msg.sender, _collection, _id);
+        delete s_NFTPrices[_collection][_id];
+        emit ListingCanceled(_collection, _id);
     }
 
 
@@ -68,17 +77,17 @@ contract Marketplace {
     isNFTOwner(_collection, _id) 
     alreadyListed(_collection, _id, false)
     {
-        require(NFTPrices[_collection][_id] < _newPrice);
-        NFTPrices[_collection][_id] = _newPrice;
-        emit NFTUpdated(msg.sender, _collection, _id);        
+        if(s_NFTPrices[_collection][_id] >= _newPrice) revert newPriceNotHigher();
+        s_NFTPrices[_collection][_id] = _newPrice;
+        emit ListingUpdated(_collection, _newPrice, _id);        
     }
 
 
     function buyNFT(address _collection, uint256 _id) payable public {
         NFT currentCollection = NFT(_collection);
-        uint256 _price = NFTPrices[_collection][_id];
-        require(currentCollection.getApproved(_id) == address(this), "Marketplace is not approved");
-        require(msg.value == _price, "Invalid price");
+        uint256 _price = s_NFTPrices[_collection][_id];
+        if (currentCollection.getApproved(_id) != address(this)) revert marketplaceIsNotApproved();
+        if (msg.value != _price) revert incorectPriceProvided();
         address currentOwner = currentCollection.ownerOf(_id);
         
         currentCollection.safeTransferFrom(
@@ -89,6 +98,6 @@ contract Marketplace {
 
         emit NFTTransfered(currentOwner, msg.sender, _collection, _id);
         (bool succes,) = currentOwner.call{value: _price}("");
-        require(succes, "Error paying old owner");
+        if(!succes) revert couldntPayOldOwner();
     }
 }
