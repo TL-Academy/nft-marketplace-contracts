3c88c60cc2dfb879926d07266703e74df288a69f
Darin Ivanov <d.v.ivanov98@gmail.com>
2023-11-20 12:51:20 +0200

    can't receive implemented

--- test/Marketplace.t.sol
+++ test/Marketplace.t.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.20;
 import {Test, console2} from "../lib/forge-std/src/Test.sol";
 import "../src/Marketplace.sol";
 import {NFT} from "../src/NFT.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
 contract MarketplaceTest is Test {
     NFT public nft;
@@ -149,39 +150,49 @@ contract MarketplaceTest is Test {
 
     // TODO: Not working - needs fix
     function testBuyNFTCouldntPayOldOwner() public {
-        // CantReceive receiver = new CantReceive();
-        // vm.prank(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
-        // nft.mint("test");
-        // vm.prank(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
-        // nft.approve(address(marketplace), 3);
-        // vm.prank(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
-        // marketplace.list(address(nft), 3, 5000);
-        // vm.prank(0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f);
-        // vm.deal(0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f, 1 ether);
-        // address(receiver).call{value: 10000}("fallback");
-        // receiver.buyNFT{value: 5000}(address(marketplace), address(nft), 3);
-        // bytes4 selector = bytes4(
-        //     keccak256("Marketplace__couldntPayOldOwner()")
-        // );
-        // vm.expectRevert(abi.encodeWithSelector(selector));
-        // vm.prank(0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f);
-        // vm.deal(0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f, 1 ether);
-        // marketplace.buyNFT{value: 5000}(address(nft), 3);
+        CantReceive receiver = new CantReceive();
+        vm.prank(otherAddr);
+        nft.mint("test");
+        vm.prank(otherAddr);
+        nft.approve(address(marketplace), 3);
+        vm.prank(otherAddr);
+        marketplace.list(address(nft), 3, 5000);
+        vm.prank(otherAddr);
+        vm.deal(otherAddr, 1 ether);
+        (bool success,) = address(receiver).call{value: 10000}("fallback");
+        if (success) {
+            receiver.buyNFT{value: 5000}(address(marketplace), address(nft), 3);
+            receiver.approve(address(marketplace), address(nft), 3);
+            bytes4 selector = bytes4(
+                keccak256("Marketplace__couldntPayOldOwner()")
+            );
+            vm.expectRevert(abi.encodeWithSelector(selector));
+            vm.prank(otherAddr);
+            marketplace.buyNFT{value: 5000}(address(nft), 3);
+        }
     }
 }
 
-// contract CantReceive {
-//     function buyNFT(
-//         address _marketplace,
-//         address _collection,
-//         uint256 _id
-//     ) public payable {
-//         Marketplace(_marketplace).buyNFT{value: msg.value}(_collection, _id);
-//     }
+contract CantReceive is IERC721Receiver {
+    function buyNFT(
+        address _marketplace,
+        address _collection,
+        uint256 _id
+    ) public payable {
+        Marketplace(_marketplace).buyNFT{value: msg.value}(_collection, _id);
+    }
 
-//     receive() external payable {
-//         revert();
-//     }
+    function approve(address _marketplace, address _nft, uint256 _id) public {
+        NFT(_nft).approve(_marketplace, _id);
+    }
 
-//     fallback() external payable {}
-// }
+    receive() external payable {
+        revert();
+    }
+
+    fallback() external payable {}
+
+    function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    }
+}
